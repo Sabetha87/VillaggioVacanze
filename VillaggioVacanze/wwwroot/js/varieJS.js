022e16943f76f64bb1e4f49e7bd94dcf51b9e842
Dagniele3 <daniele.caprari3@gmail.com>
2023-06-14 17:54:52 +0200

    fix function reveal on scroll and on load

--- VillaggioVacanze/wwwroot/js/varieJS.js
+++ VillaggioVacanze/wwwroot/js/varieJS.js
@@ -1,6 +1,20 @@
-﻿function reveal() {
+﻿//function reveal() {
+//    var reveals = document.querySelectorAll(".reveal");
+//    for (var i = 0; i < reveals.length; i++) {
+//        var windowHeight = window.innerHeight;
+//        var elementTop = reveals[i].getBoundingClientRect().top;
+//        var elementVisible = 150;
+//        if (elementTop < windowHeight - elementVisible) {
+//            reveals[i].classList.add("active");
+//        } else {
+//            reveals[i].classList.remove("active");
+//        }
+//    }
+//}
+
+function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
+    for (var i = 1; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
         var elementVisible = 150;
@@ -12,4 +26,8 @@
     }
 }
 
+
+window.onload = (event) => {
+    document.querySelectorAll(".reveal")[0].classList.add("active");
+}
 window.addEventListener("scroll", reveal);
